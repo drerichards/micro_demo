b3d83e4ab968dc42378a0cf3505470ba4c394ca8
Andre <g.andrerichards@gmail.com>
2021-08-22 18:28:09 -0400

    refactor code

--- marketing/config/webpack.common.js
+++ marketing/config/webpack.common.js
@@ -1,17 +1,17 @@
 module.exports = {
-    module: {
-        rules: [
-            {
-                test: /\.m?js$/, // when loading in .js file, should be processed by babel
-                exclude: /node_modules/, // do not run on this dir
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['@babel/preset-react', '@babel/preset-env'],
-                        plugins: ['@babel/plugin-transform-runtime'],
-                    }
-                }
-            }
-        ]
-    }
-}
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-react', '@babel/preset-env'],
+            plugins: ['@babel/plugin-transform-runtime'],
+          },
+        },
+      },
+    ],
+  },
+};
